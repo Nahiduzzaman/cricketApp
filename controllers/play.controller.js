8f3b7ba03b42f047047917994415bfa5b8eadf91
Md Nahiduzzaman Rose <nahid.uzzaman@selise.com.bd>
2017-02-22 19:10:41 +0600

    cricketApp

---
+++ controllers/play.controller.js
@@ -0,0 +1,40 @@
+﻿(function(angular) {
+'use strict';
+	
+	angular.module('cricketApp')
+		.controller('playController', constructor);
+		    constructor.$inject = ['$stateParams','StatisticService','$scope','$window','$state'];
+		    function constructor($stateParams,StatisticService,$scope,$window,$state) {
+		    	var gameData = [];
+		        $scope.resetData = function(){
+		            console.log('removeItem');
+		            $scope.gameData = localStorage.removeItem("gameData");
+		            $scope.teamData = localStorage.removeItem("teamData");
+		            $window.location.reload();
+		        }
+		      	
+		      	$scope.teamData = JSON.parse(localStorage.getItem("teamData"));
+		      	console.log('teamData',$scope.teamData);
+		      
+		      	$scope.bowl = function(){
+		      		gameData = StatisticService.play();
+		      		localStorage.setItem("gameData", JSON.stringify(gameData));
+		            $scope.gameData = JSON.parse(localStorage.getItem("gameData"));
+		      		console.log('gameData',$scope.gameData);
+		            if($scope.gameData[$scope.gameData.length-1].score == 'WD' || $scope.gameData[$scope.gameData.length-1].score == 'NB' ){
+		            $state.reload();
+		          }              
+		      	}
+
+		        $scope.gameData = JSON.parse(localStorage.getItem("gameData")); //it shows when controller reinitialize after route changed
+
+		        $scope.sendIndex = function(item,idx){
+		            $scope.getStats = item;
+		            console.log('getStats in  sendIndex',$scope.getStats);
+		        }
+		 
+		      	$scope.getStats = StatisticService.get($stateParams.ball,$stateParams.over,$scope.gameData);
+		      	console.log('getStats',$scope.getStats); 
+		    }
+
+})(window.angular);
---
+++ controllers/start.controller.js
@@ -0,0 +1,26 @@
+﻿(function (angular) {
+'use strict';
+
+	angular.module('cricketApp')
+	    .controller('startController', constructor);
+	    	constructor.$inject = ['StatisticService','$scope'];
+	    	function constructor(StatisticService,$scope) {
+	    		var vm = this;
+	        	$scope.game = {
+		     		country1: 'Bangladesh',
+		     		country2: 'India',     		
+		     		to_bowl: null
+		     	};
+
+		     	$scope.setGame = function(game){
+		     		if(game.to_bowl == game.country1){
+				  		game.to_bat = game.country2;
+				  	}else{
+				  		game.to_bat = game.country1;
+				  	}
+				  	console.log(game);
+					localStorage.setItem("teamData", JSON.stringify(game));
+		     	}
+	    	}
+
+})(window.angular);
