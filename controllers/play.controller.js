35583f955ce4fd22eef4f6d78e6d42d749e30fb9
Md. Nahiduzzaman Rose <Md. Nahiduzzaman Rose>
2017-02-22 23:25:02 +0600

    vm in both controller

--- controllers/play.controller.js
+++ controllers/play.controller.js
@@ -5,36 +5,37 @@
 		.controller('playController', constructor);
 		    constructor.$inject = ['$stateParams','StatisticService','$scope','$window','$state'];
 		    function constructor($stateParams,StatisticService,$scope,$window,$state) {
+		    	var vm = this;
 		    	var gameData = [];
-		        $scope.resetData = function(){
+		        vm.resetData = function(){
 		            console.log('removeItem');
-		            $scope.gameData = localStorage.removeItem("gameData");
-		            $scope.teamData = localStorage.removeItem("teamData");
+		            vm.gameData = localStorage.removeItem("gameData");
+		            vm.teamData = localStorage.removeItem("teamData");
 		            $window.location.reload();
 		        }
 		      	
-		      	$scope.teamData = JSON.parse(localStorage.getItem("teamData"));
-		      	console.log('teamData',$scope.teamData);
+		      	vm.teamData = JSON.parse(localStorage.getItem("teamData"));
+		      	console.log('teamData',vm.teamData);
 		      
-		      	$scope.bowl = function(){
+		      	vm.bowl = function(){
 		      		gameData = StatisticService.play();
 		      		localStorage.setItem("gameData", JSON.stringify(gameData));
-		            $scope.gameData = JSON.parse(localStorage.getItem("gameData"));
-		      		console.log('gameData',$scope.gameData);
-		            if($scope.gameData[$scope.gameData.length-1].score == 'WD' || $scope.gameData[$scope.gameData.length-1].score == 'NB' ){
+		            vm.gameData = JSON.parse(localStorage.getItem("gameData"));
+		      		console.log('gameData',vm.gameData);
+		            if(vm.gameData[vm.gameData.length-1].score == 'WD' || vm.gameData[vm.gameData.length-1].score == 'NB' ){
 		            $state.reload();
 		          }              
 		      	}
 
-		        $scope.gameData = JSON.parse(localStorage.getItem("gameData")); //it shows when controller reinitialize after route changed
+		        vm.gameData = JSON.parse(localStorage.getItem("gameData")); //it shows when controller reinitialize after route changed
 
-		        $scope.sendIndex = function(item,idx){
-		            $scope.getStats = item;
-		            console.log('getStats in  sendIndex',$scope.getStats);
+		        vm.sendIndex = function(item,idx){
+		            vm.getStats = item;
+		            console.log('getStats in  sendIndex',vm.getStats);
 		        }
 		 
-		      	$scope.getStats = StatisticService.get($stateParams.ball,$stateParams.over,$scope.gameData);
-		      	console.log('getStats',$scope.getStats); 
+		      	vm.getStats = StatisticService.get($stateParams.ball,$stateParams.over,vm.gameData);
+		      	console.log('getStats',vm.getStats); 
 		    }
 
 })(window.angular);
